083095cc7fe459db6731aa1b65a718c35bed1680
alannaedey <37161994+alannaedey@users.noreply.github.com>
2018-04-25 15:30:43 +1000

    Add files via upload

---
+++ CounterTest/CounterTest/Clock.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace CounterTest
+{
+    public class Clock
+    {
+        private Counter[] _counters;
+
+
+        public Clock()
+        {
+            _counters = new Counter[3];
+            _counters[0] = new Counter("Seconds");
+            _counters[1] = new Counter("Minutes");
+            _counters[2] = new Counter("Hours");
+        }
+
+
+        public void Tick()
+        {
+            _counters[0].Increment();
+            if (_counters[0].Value == 60)
+            {
+                _counters[0].Reset();
+                _counters[1].Increment();
+            }
+
+            if (_counters[1].Value == 60)
+            {
+                _counters[1].Reset();
+                _counters[2].Increment();
+            }
+
+            if (_counters[2].Value == 24)
+            {
+                _counters[2].Reset();
+            }
+        }
+
+
+        public void Reset()
+        {
+            _counters[0].Reset();
+            _counters[1].Reset();
+            _counters[2].Reset();
+        }
+
+        public string Value{
+            get{
+                return String.Format("{0:00}:{1:00}:{2:00}", _counters[2].Value, _counters[1].Value, _counters[0].Value );
+            }
+        }
+    }
+
+
+}
