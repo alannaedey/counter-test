90df673ad85627ea5db40fd5f4be1adfb1dc818f
alannaedey <37161994+alannaedey@users.noreply.github.com>
2018-04-25 15:44:19 +1000

    Add files via upload

---
+++ CounterTest/CounterTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CounterTest
+{
+    class MainClass
+    {
+
+        public static void Main(string[] args)
+        {
+            Clock clock = new Clock();
+
+            for (int i = 0; i < 56700; i++)
+            {
+                clock.Tick();
+            }
+
+            Console.WriteLine(clock.Value);
+
+            clock.Reset();
+
+            for (int i = 0; i < 23456; i++)
+            {
+                clock.Tick();
+            }
+
+            Console.WriteLine(clock.Value);
+        }
+
+
+
+    }
+}
---
+++ CounterTest/CounterTest/UnitTests/ClockTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using NUnit.Framework;
+namespace CounterTest
+{
+    [TestFixture]
+    public class ClockTest
+    {
+        [Test]
+        public void TestInitialise()
+        {
+            Clock clock = new Clock();
+
+            if (clock.Value != "00:00:00")
+            {
+                Assert.Fail("Initialise Test has Failed!");
+            }
+        }
+
+        [Test]
+        public void TestTickSecToMin()
+        {
+            Clock clock = new Clock();
+            for (int i = 0; i < 60; i++)
+            {
+                clock.Tick();
+            }
+            if (clock.Value != "00:01:00")
+            {
+                Assert.Fail("Tick Sec To Min Test has Failed!");
+            }
+        }
+
+        [Test]
+        public void TestTickMinToHour()
+        {
+            Clock clock = new Clock();
+            for (int i = 0; i < 3600; i++)
+            {
+                clock.Tick();
+            }
+            if (clock.Value != "01:00:00")
+            {
+                Assert.Fail("Tick Test Min To Hour has Failed!");
+            }
+        }
+
+        [Test]
+        public void TestTickHour()
+        {
+            Clock clock = new Clock();
+            for (int i = 0; i < 86400; i++)
+            {
+                clock.Tick();
+            }
+            if (clock.Value != "00:00:00")
+            {
+                Assert.Fail("Tick Test Hour has Failed!");
+            }
+        }
+
+
+        [Test]
+        public void TestReset()
+        {
+            Clock clock = new Clock();
+            clock.Tick();
+            clock.Tick();
+            clock.Tick();
+            clock.Reset();
+
+            if (clock.Value != "00:00:00")
+            {
+                Assert.Fail("Reset Test has Failed!");
+            }
+        }
+    }
+}
